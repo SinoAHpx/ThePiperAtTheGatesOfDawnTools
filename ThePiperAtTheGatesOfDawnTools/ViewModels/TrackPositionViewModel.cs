1b05f48eba923c4a26677dcd9003837997c05c78
AHpx <ahpx@yandex.com>
2023-05-01 10:59:13 +0800

    Simplified logic

--- ThePiperAtTheGatesOfDawnTools/ViewModels/TrackPositionViewModel.cs
+++ ThePiperAtTheGatesOfDawnTools/ViewModels/TrackPositionViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reactive;
 using System.Text;
 using Avalonia;
+using Manganese.Text;
 using ReactiveUI;
 
 namespace ThePiperAtTheGatesOfDawnTools.ViewModels;
@@ -39,17 +40,21 @@ public class TrackPositionViewModel : ViewModelBase
 
     public void Parse()
     {
+        if (RawTracks.IsNullOrEmpty())
+        {
+            return;
+        }
         var lines = RawTracks.Split('\n');
         var totalSeconds = 0;
         var output = new StringBuilder();
         foreach (var line in lines)
         {
-            if (!line.Contains("(") || !line.Contains(")"))
+            if (!line.Contains('(') || !line.Contains(')'))
                 continue;
             var lastOpenParenthesisIndex = line.LastIndexOf('(');
             var lastCloseParenthesisIndex = line.LastIndexOf(')');
             var duration = line.Substring(lastOpenParenthesisIndex + 1, lastCloseParenthesisIndex - lastOpenParenthesisIndex - 1);
-            if (!duration.Contains(":"))
+            if (!duration.Contains(':'))
                 continue;
             var durationParts = duration.Split(':');
             if (!int.TryParse(durationParts[0], out var minutes) || !int.TryParse(durationParts[1], out var seconds))
@@ -60,18 +65,11 @@ public class TrackPositionViewModel : ViewModelBase
         
         ParsedTracks = output.ToString();
         IsParsed = true;
-    }
-
-    public ReactiveCommand<Unit, Unit> CopyCommand { get; set; }
-
-    public void Copy()
-    {
         Application.Current?.Clipboard?.SetTextAsync(ParsedTracks);
     }
 
     public TrackPositionViewModel()
     {
         ParseCommand = ReactiveCommand.Create(Parse);
-        CopyCommand = ReactiveCommand.Create(Copy);
     }
 }
